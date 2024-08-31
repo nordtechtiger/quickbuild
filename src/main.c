b1052d363af1e0547f446f04ea426dc738e1c8ce
Jake <32938014+redtechtiger@users.noreply.github.com>
2024-08-31 13:48:24 -0400

    Work: Read config file bytes and add lexer files

--- src/main.c
+++ src/main.c
@@ -1,4 +1,5 @@
 #include "fs_tools.h"
+#include "lexer.h"
 #include "stdio.h"
 #include "stdlib.h"
 
@@ -11,21 +12,30 @@ int main(int argc, char **argv) {
   struct FsObject *fs_tree = calloc(1, sizeof(struct FsObject));
   int ret = get_fs_object(".", fs_tree);
   if (0 > ret) {
-    printf("= error: failed to get filesystem information. consider submitting "
-           "a bug report");
+    printf("= error: failed to get directory tree. this is likely a bug, "
+           "consider submitting a bug report.\n");
     return EXIT_FAILURE;
   }
 
-  // debug: print all paths
-  // while(1) {
-  //   printf("path `%s` is file `%s`\n", fs_tree->path, fs_tree->name);
-  //   if(!(fs_tree = fs_tree->child)) break;
-  // }
+  // load config file into memory (rb = read binary)
+  FILE *config_file = fopen(CONFIG_FILE, "rb");
+  if (config_file == NULL) {
+    printf("= error: couldn't find a quickbuild config file.\n");
+    return EXIT_FAILURE;
+  }
+  fseek(config_file, 0, SEEK_END);
+  size_t config_size = ftell(config_file);
+  fseek(config_file, 0, SEEK_SET);
 
-  // load config file
+  uint8_t *config_bytes = calloc(config_size, 1);
+  fread(config_bytes, 1, config_size, config_file);
+
+  fclose(config_file);
+
+  // lex config
 
   // clean up and exit
-  // free_fs_object(fs_object);
+  free_fs_object(fs_tree);
   printf("= done\n");
   return EXIT_SUCCESS;
 }
