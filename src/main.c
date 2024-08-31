2cd92870acbb0e1f382866d19df426b0bd63d8be
Jake <32938014+redtechtiger@users.noreply.github.com>
2024-08-30 23:05:46 -0400

    Tidy

--- src/main.c
+++ src/main.c
@@ -11,7 +11,8 @@ int main(int argc, char **argv) {
   struct FsObject *fs_tree = calloc(1, sizeof(struct FsObject));
   int ret = get_fs_object(".", fs_tree);
   if (0 > ret) {
-    printf("= error: failed to get filesystem information. consider submitting a bug report");
+    printf("= error: failed to get filesystem information. consider submitting "
+           "a bug report");
     return EXIT_FAILURE;
   }
 
@@ -22,11 +23,7 @@ int main(int argc, char **argv) {
   // }
 
   // load config file
-  struct FsObject *config_file;
-  config_file = find_fs_objects("./src/*.c", 2, fs_tree);
-  
 
-  
   // clean up and exit
   // free_fs_object(fs_object);
   printf("= done\n");
