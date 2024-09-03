24dd871059aaab2b51afdab28ea22124fe880504
Jake <32938014+redtechtiger@users.noreply.github.com>
2024-09-03 12:27:34 -0400

    Work: Initial code for removing spaces and newlines

--- src/lexer.cpp
+++ src/lexer.cpp
@@ -6,5 +6,15 @@ using namespace std;
 Lexer::Lexer() { this->lex_state = NONE; }
 
 vector<Token> Lexer::lex_bytes(const vector<unsigned char> input_bytes) {
-
+  cout << this->lex_state;
+  for (auto const &character : input_bytes) {
+    if (this->lex_state == NONE && (character == ' ' || character == '\n')) {
+      continue;
+    }
+    if (character == '\"')
+      this->lex_state = (this->lex_state == NONE ? IN_STRING : NONE);
+    cout << character;
+  }
+  // flush
+  cout << endl;
 }
