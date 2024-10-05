5e6cf8f48ec9215e9825fefa38f1ac29d17b39f9
RedTechTiger <32938014+redtechtiger@users.noreply.github.com>
2024-10-05 19:05:57 -0400

    Work: Finish comments for AST layout

--- src/parser.cpp
+++ src/parser.cpp
@@ -1,5 +1,6 @@
 #include "parser.hpp"
 #include "lexer.hpp"
+#include <iostream>
 
 using namespace std;
 
@@ -12,7 +13,6 @@ const vector<int (*)(vector<Token>)> parsing_rules{
 };
 
 AST Parser::parse_tokens(std::vector<Token> token_stream) {
-  
 }
 
 /* # Assuming current config, the AST should look like the following:
@@ -21,5 +21,14 @@ AST Parser::parse_tokens(std::vector<Token> token_stream) {
  * field("flags", <Literal"-g -o0">)
  * field("sources", <Literal"src/*.c">)
  * field("headers", <Literal"src/*.h">)
- * field("objects", <
+ * field("objects", <Replace"sources""src/*.c""obj/*.o">)
+ * field("binary", <Literal"bin/quickbuild">)
+ * target(Literal"quickbuild", "quickbuild", <
+ *   field("depends", <Variable"objects", Variable"headers">)
+ *   field("run", <Concatenation<Variable"compiler",Literal" ", ...etc>>)
+ * >)
+ * target(Variable"objects", "obj", <
+ *   field("depends", <Replace"obj"obj/*.o""src/*.c">)
+ *   field("run", <Concatenation<Variable"compiler",Literal" ", ...etc>>)
+ * >)
  */
