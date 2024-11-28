5d33a58bb61187a3af8426a0264ce8d6e6097c0a
RedTechTiger <32938014+redtechtiger@users.noreply.github.com>
2024-11-28 16:24:38 -0500

    Work: Tidy up the driver by e.g. changing the defines for logging

--- src/driver.cpp
+++ src/driver.cpp
@@ -1,4 +1,65 @@
+// std::string debug_token_to_string(Token token) {
+//   std::string out;
+//   switch (token.type) {
+//   case TokenType::Identifier:
+//     out = "Identifier";
+//     break;
+//   case TokenType::Literal:
+//     out = "Literal";
+//     break;
+//   case TokenType::Equals:
+//     out = "Equals";
+//     break;
+//   case TokenType::Modify:
+//     out = "Modify";
+//     break;
+//   case TokenType::LineStop:
+//     out = "LineStop";
+//     break;
+//   case TokenType::Arrow:
+//     out = "Arrow";
+//     break;
+//   case TokenType::IterateAs:
+//     out = "IterateAs";
+//     break;
+//   case TokenType::Separator:
+//     out = "Separator";
+//     break;
+//   case TokenType::ExpressionOpen:
+//     out = "ExpressionOpen";
+//     break;
+//   case TokenType::ExpressionClose:
+//     out = "ExpressionClose";
+//     break;
+//   case TokenType::TargetOpen:
+//     out = "TargetOpen";
+//     break;
+//   case TokenType::TargetClose:
+//     out = "TargetClose";
+//     break;
+//   case TokenType::ConcatLiteral:
+//     out = "ConcatLiteral";
+//     break;
+//   case TokenType::Invalid:
+//     out = "Invalid";
+//     break;
+//   }
+//   if (token.context)
+//     out += ":`" + *token.context + "`";
+//   return out;
+// }
+// 
+// void debug_print_tokens(std::vector<Token> token_stream) {
+//   for (const auto &token : token_stream) {
+//     std::cout << "[" << debug_token_to_string(token) << "] ";
+//     if (token.type == TokenType::LineStop)
+//       std::cout << std::endl;
+//   }
+//   std::cout << std::endl;
+// }
+
 #include "driver.hpp"
+#include "builder.hpp"
 #include "lexer.hpp"
 #include "parser.hpp"
 
@@ -6,8 +67,18 @@
 #include <iostream>
 #include <iterator>
 
-#define LL LoggingLevel
-#define LL_SETUP m_setup.logging_level
+#define LOG_VERBOSE(msg)                                                       \
+  if (m_setup.logging_level >= LoggingLevel::Verbose) {                        \
+    std::cout << msg << std::endl;                                             \
+  }
+#define LOG_STANDARD(msg)                                                      \
+  if (m_setup.logging_level >= LoggingLevel::Standard) {                       \
+    std::cout << msg << std::endl;                                             \
+  }
+#define LOG_QUIET(msg)                                                         \
+  if (m_setup.logging_level >= LoggingLevel::Quiet) {                          \
+    std::cout << msg << std::endl;                                             \
+  }
 #define CONFIG_FILE "./quickbuild"
 
 Driver::Driver(Setup setup) { m_setup = setup; }
@@ -36,86 +107,26 @@ std::vector<unsigned char> Driver::get_config() {
   throw DriverException("Driver-D001: Failed to get config.");
 }
 
-std::string debug_token_to_string(Token token) {
-  std::string out;
-  switch (token.type) {
-  case TokenType::Identifier:
-    out = "Identifier";
-    break;
-  case TokenType::Literal:
-    out = "Literal";
-    break;
-  case TokenType::Equals:
-    out = "Equals";
-    break;
-  case TokenType::Modify:
-    out = "Modify";
-    break;
-  case TokenType::LineStop:
-    out = "LineStop";
-    break;
-  case TokenType::Arrow:
-    out = "Arrow";
-    break;
-  case TokenType::IterateAs:
-    out = "IterateAs";
-    break;
-  case TokenType::Separator:
-    out = "Separator";
-    break;
-  case TokenType::ExpressionOpen:
-    out = "ExpressionOpen";
-    break;
-  case TokenType::ExpressionClose:
-    out = "ExpressionClose";
-    break;
-  case TokenType::TargetOpen:
-    out = "TargetOpen";
-    break;
-  case TokenType::TargetClose:
-    out = "TargetClose";
-    break;
-  case TokenType::ConcatLiteral:
-    out = "ConcatLiteral";
-    break;
-  case TokenType::Invalid:
-    out = "Invalid";
-    break;
-  }
-  if (token.context)
-    out += ":`" + *token.context + "`";
-  return out;
-}
 
-void debug_print_tokens(std::vector<Token> token_stream) {
-  for (const auto &token : token_stream) {
-    std::cout << "[" << debug_token_to_string(token) << "] ";
-    if (token.type == TokenType::LineStop)
-      std::cout << std::endl;
-  }
-  std::cout << std::endl;
-}
 
 int Driver::run() {
   // Out: Greeting
-  if (LL_SETUP >= LL::Standard) {
-    std::cout << "@ Quickbuild Dev v0.1.0" << std::endl;
-  }
+  LOG_STANDARD("[ Quickbuild Dev v0.1.0 ]");
 
+  // === Build the configuration ===
   // Out: Compiling config
-  if (LL_SETUP >= LL::Standard) {
-    std::cout << "=> Building configuration..." << std::endl;
-  }
-
+  LOG_STANDARD("=> Building configuration...");
   // Get config
   std::vector<unsigned char> config = get_config();
-
   // Lex
   Lexer lexer(config);
-  std::vector<Token> t_stream =
-      lexer.get_token_stream(); // TODO: Handle properly
-  // debug_print_tokens(t_stream);
-
+  std::vector<Token> t_stream;
+  try {
+    t_stream = lexer.get_token_stream();
+  } catch (LexerException e) {
+    // TODO: Handle properly
+    std::cerr << e.what();
+  }
   // Parse
   AST ast;
   try {
@@ -125,9 +136,19 @@ int Driver::run() {
     // TODO: Handle properly
     // throw;
     std::cerr << e.what();
-    return -1;
+    return EXIT_FAILURE;
   }
 
-  std::cerr << ">>> Driver exiting normally." << std::endl;
-  return EXIT_FAILURE; // TODO: Replace this when driver is finished
+  // === Build the project ===
+  // Build
+  Builder builder(ast, m_setup);
+  try {
+    builder.build();
+  } catch (BuilderException e) {
+    // TODO: Handle properly
+    std::cerr << e.what();
+    return EXIT_FAILURE;
+  }
+
+  return EXIT_SUCCESS; // TODO: Replace this when driver is finished
 }
