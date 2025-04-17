9b356545e85c983172fa5e58e8ec895026382cd6
nordtechtiger <32938014+nordtechtiger@users.noreply.github.com>
2025-04-16 21:00:37 -0400

    wip: add synchronisation to io

--- src/format.hpp
+++ src/format.hpp
@@ -2,6 +2,7 @@
 #define FORMAT_H
 
 #include <iostream>
+#include <mutex>
 #include <ostream>
 #include <unistd.h>
 
@@ -13,29 +14,43 @@
 #define RESET (isatty(STDOUT_FILENO) ? "\033[0m" : "")
 #define ITALIC (isatty(STDOUT_FILENO) ? "\033[3m" : "")
 
+static std::mutex io_lock;
+
 #define LOG_VERBOSE(msg)                                                       \
   if (m_setup.logging_level >= LoggingLevel::Verbose) {                        \
+    io_lock.lock();                                                            \
     std::cout << msg << std::endl;                                             \
+    io_lock.unlock();                                                          \
   }
 #define LOG_STANDARD(msg)                                                      \
   if (m_setup.logging_level >= LoggingLevel::Standard) {                       \
+    io_lock.lock();                                                            \
     std::cout << msg << std::endl;                                             \
+    io_lock.unlock();                                                          \
   }
 #define LOG_QUIET(msg)                                                         \
   if (m_setup.logging_level >= LoggingLevel::Quiet) {                          \
+    io_lock.lock();                                                            \
     std::cout << msg << std::endl;                                             \
+    io_lock.unlock();                                                          \
   }
 #define LOG_VERBOSE_NO_NEWLINE(msg)                                            \
   if (m_setup.logging_level >= LoggingLevel::Verbose) {                        \
+    io_lock.lock();                                                            \
     std::cout << msg << std::flush;                                            \
+    io_lock.unlock();                                                          \
   }
 #define LOG_STANDARD_NO_NEWLINE(msg)                                           \
   if (m_setup.logging_level >= LoggingLevel::Standard) {                       \
+    io_lock.lock();                                                            \
     std::cout << msg << std::flush;                                            \
+    io_lock.unlock();                                                          \
   }
 #define LOG_QUIET_NO_NEWLINE(msg)                                              \
   if (m_setup.logging_level >= LoggingLevel::Quiet) {                          \
+    io_lock.lock();                                                            \
     std::cout << msg << std::flush;                                            \
+    io_lock.unlock();                                                          \
   }
 
 #endif
